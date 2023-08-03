4ff171a15753debd6aeff471ed9b85dc3d666e1b
Arjun Puri <arjunpuri@arjunpuri.me>
2023-08-03 10:21:56 +0530

    basic ssr implementation of the same posts page.

---
+++ src/pages/ssrPost/[slug].jsx
@@ -0,0 +1,48 @@
+import { PortableText } from '@portabletext/react'
+
+import Container from '~/components/Container'
+import { readToken } from '~/lib/sanity.api'
+import { getClient } from '~/lib/sanity.client'
+import {
+  getPost,
+  postBySlugQuery,
+} from '~/lib/sanity.queries'
+import { formatDate } from '~/utils'
+
+export async function getServerSideProps({ draftMode = false, params = {} }) {
+  const client = getClient(draftMode ? { token: readToken } : undefined)
+  const post = await getPost(client, params.slug)
+
+  if (!post) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      draftMode,
+      token: draftMode ? readToken : '',
+      post,
+    },
+  }
+}
+
+export default function ProjectSlugRoute(props) {
+  const post = props.post
+
+  return (
+    <Container>
+      <section className="post">
+        <div className="post__container">
+          <h1 className="post__title">{post.title}</h1>
+          <p className="post__excerpt">{post.excerpt}</p>
+          <p className="post__date">{formatDate(post._createdAt)}</p>
+          <div className="post__content">
+            <PortableText value={post.body} />
+          </div>
+        </div>
+      </section>
+    </Container>
+  )
+}
